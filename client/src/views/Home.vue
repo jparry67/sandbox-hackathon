b78ac9238af6d429dd1e6f5974f8705e2446cfe6
Jacob Parry <jacob@parry.co>
2021-04-08 21:52:36 -0600

    testing vue deployment

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld :msg="message"/>
+        <h1>Welcome to the Sandbox Hackathon Project</h1>
+        <h3>Check out the todos</h3>
     </div>
 </template>
 
